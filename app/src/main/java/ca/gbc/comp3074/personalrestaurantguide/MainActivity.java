df4990dbe119e011c7400275eb0e8633aa48a102
Berantzino <lasse@berantzino.dk>
2020-11-21 01:41:39 +0100

    adds project files

---
+++ app/src/main/java/ca/gbc/comp3074/personalrestaurantguide/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.gbc.comp3074.personalrestaurantguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
