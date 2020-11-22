de13054aefea76e91f3fe2619513cd422a0693d0
Berantzino <lasse@berantzino.dk>
2020-11-23 00:33:56 +0100

    fixes auto-merge errors, changes launcher activity and refactoring.

--- app/src/main/java/ca/gbc/comp3074/personalrestaurantguide/activity/SearchActivity.java
+++ app/src/main/java/ca/gbc/comp3074/personalrestaurantguide/activity/SearchActivity.java
@@ -1,43 +1,42 @@
-package ca.gbc.comp3074.personalrestaurantguide;
+package ca.gbc.comp3074.personalrestaurantguide.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import ca.gbc.comp3074.personalrestaurantguide.R;
+
+public class SearchActivity extends AppCompatActivity {
 
     private Spinner restaurantType;
     private EditText searchText;
-    private Button Search;
+    private Button search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
 
          restaurantType = findViewById(R.id.spinner);
          searchText =  findViewById(R.id.search);
-         Search =  findViewById(R.id.button);
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_expandable_list_item_1,
+         search =  findViewById(R.id.button);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(SearchActivity.this, android.R.layout.simple_expandable_list_item_1,
                 getResources().getStringArray(R.array.restauranttypes));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         restaurantType.setAdapter(myAdapter);
 
-        Search.setOnClickListener(new View.OnClickListener() {
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent  =  new Intent(MainActivity.this,RestaurantListActivity.class);
+                Intent intent  =  new Intent(SearchActivity.this, RestaurantListActivity.class);
                 intent.putExtra("searchtext",searchText.getText().toString());
                 intent.putExtra("type", restaurantType.getSelectedItem().toString());
                 startActivity(intent);
-
-
             }
         });
 
