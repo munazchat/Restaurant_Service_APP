bd856368dc14ae61f09d745065b6342781c0fa5f
Berantzino <lasse@berantzino.dk>
2020-11-23 01:05:56 +0100

    adds rough ui for admin page

---
+++ app/src/main/java/ca/gbc/comp3074/personalrestaurantguide/activity/AdminActivity.java
@@ -0,0 +1,16 @@
+package ca.gbc.comp3074.personalrestaurantguide.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.gbc.comp3074.personalrestaurantguide.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
